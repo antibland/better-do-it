2b35a5d13a44f8a26b1f2a4f816b5881a5cbe1ec
Andy Hoffman <antibland@gmail.com>
2025-08-29 00:40:45 -0400

    feat/og

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,25 @@ export const metadata: Metadata = {
     "apple-mobile-web-app-capable": "yes",
     "apple-mobile-web-app-status-bar-style": "default",
   },
+  // Open Graph metadata for social media sharing
+  openGraph: {
+    title: "Better Do It - Collaborative Todo App",
+    description:
+      "The collaborative todo app designed for partners. Plan, track, and accomplish your goals together.",
+    type: "website",
+    locale: "en_US",
+    siteName: "Better Do It",
+    // The opengraph-image.png file in the app directory will be automatically used
+    // Next.js will generate the appropriate og:image meta tags
+  },
+  // Twitter Card metadata
+  twitter: {
+    card: "summary_large_image",
+    title: "Better Do It - Collaborative Todo App",
+    description:
+      "The collaborative todo app designed for partners. Plan, track, and accomplish your goals together.",
+    // The opengraph-image.png will also be used for Twitter cards
+  },
 };
 
 export default function RootLayout({
